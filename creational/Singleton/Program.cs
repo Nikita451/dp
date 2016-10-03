f9e6aeb0ecd623a16da60ed662c951743cafa29e
Nikita <nikita451@yandex.ru>
2016-10-03 16:00:43 +0300

    Singleton

---
+++ creational/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        class Singleton 
+        {
+            private int counter = 0;
+            private Singleton(){}
+            static private Singleton obj = null;
+            public static Singleton getInstance()
+            {
+                if ( obj == null)
+                {  
+                   obj = new Singleton();
+                   return obj; 
+                } else {
+                    return obj;
+                }
+            }
+
+            public int counterInc()
+            {
+                return ++this.counter;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Singleton st = Singleton.getInstance();
+            Console.WriteLine( st.counterInc() );
+
+            Singleton st2 = Singleton.getInstance();
+            Console.WriteLine( st2.counterInc() );
+        }
+    }
+}
