2e97c8e9f2b52479c13327dfb7a925d391029bf6
Nikita <nikita451@yandex.ru>
2016-10-03 15:43:35 +0300

    Factory Method

---
+++ creational/FactoryMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// Modern version of the pattern
+namespace ConsoleApplication
+{
+    interface IApplication
+    {
+        void run();
+    }
+
+    class WebApp : IApplication
+    {
+        public void run()
+        {
+            Console.WriteLine("running web application ...");
+        }
+    }
+
+    class DesktopApp : IApplication
+    {
+        public void run()
+        {
+            Console.WriteLine("running desktop application ...");
+        }
+    }
+
+    class CreatorApp
+    {
+        static public IApplication createApp(string app)
+        {
+            if (app == "web")
+            {
+                return new WebApp();
+            } else {
+                return new DesktopApp();
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IApplication app =  CreatorApp.createApp("web");
+            app.run();
+        }
+    }
+}
