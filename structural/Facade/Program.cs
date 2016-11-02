e342eb8ff0673c6e2f495bfb67e4cf90b4261890
Nikita <nikita451@yandex.ru>
2016-11-02 06:39:48 +0300

    Facade

---
+++ structural/Facade/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Cloud
+    {
+        public void paint(string color)
+        {
+            Console.WriteLine( String.Format("Painting a {0} cloud", color) );
+        }
+    }
+
+    class House
+    {
+        public void paint(string color)
+        {
+            Console.WriteLine( String.Format("Painting a {0} house", color) );
+        }
+    }
+
+    class Grass
+    {
+        public void paint(string color)
+        {
+            Console.WriteLine( String.Format("Painting a {0} grass", color) );
+        }
+    }
+
+    class Picture 
+    {
+        public void paint( string colorCloud, string colorHouse, string colorGrass )
+        {
+            Cloud cloud = new Cloud();
+            House house = new House();
+            Grass grass = new Grass();
+
+            cloud.paint(colorCloud);
+            house.paint(colorHouse);
+            grass.paint(colorGrass);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Picture pc = new Picture();
+            pc.paint("red", "green", "blue");
+        }
+    }
+}
